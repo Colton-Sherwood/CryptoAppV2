31ee7fed9b25b9f8f09d663888695cc071864fde
Colton Sherwood <Colton.Sherwood@southeasttech.edu>
2021-03-01 15:54:27 -0600

    Started working on SellCrypto

--- src/controllers/PurchaseController.js
+++ src/controllers/PurchaseController.js
@@ -8,80 +8,80 @@ const Purchase = require('../model/PurchaseModel')
 const CRYPTO_API_KEY = "6cbe67e231cc62448e4edd3d0b47a159"
 
 //import axios to handle api calls
-const axios = require ("axios")
+const axios = require("axios")
 
 //this will get the purchase page.
-exports.renderPurchasePage= (req, res) => {
+exports.renderPurchasePage = (req, res) => {
     //in response renders the about view
-    res.render("purchase", {layout: 'default', template: 'home-template'})
+    res.render("purchase", { layout: 'default', template: 'home-template' })
 }
 
 //use this to get all of the existing cryptocurrency purchases so the user can choose which ones they want to sell.
-exports.accountHistory = (req,res) =>{
+exports.accountHistory = (req, res) => {
     const user = req.user.email;
     //objects will store the response from the query and totalCoins will store the coin count from the mongo db in each object.
     var objects = {};
-    var totalCoins={};
+    var totalCoins = {};
     //query the purchases based on the current user logged in.
-    Purchase.find({usernameID: user }).then(history => {  
+    Purchase.find({ usernameID: user }).lean().then(history => {
         //console.log(history)
         // Reverse history so most recent comes first
-        history = history.reverse(); 
+        history = history.reverse();
         res.render('account', {
-        name: req.user.name,
-        history 
+            name: req.user.name,
+            history
         });
-   })
+    })
 };
 
 //this will handle the post request.
-exports.renderPurchase=(req, res)=>{
+exports.renderPurchase = (req, res) => {
     const purchaseID = req.body.purchaseID
     const purchase = new Crypto(purchaseID)
 
-       //set up API call to get info we want based on value entered
-       const url = `https://api.nomics.com/v1/currencies/ticker?key=${CRYPTO_API_KEY}&ids=${purchaseID}&interval=1d,30d`
-   
-       purchase.validateUserInput()
-   
-           // use axios to get info from api and do what we want with it in the then
-           axios.get(url).then((response) => {
-               // can use destructuring to get neater names for required info
-               // make a variable with the name of the data point that you want
-               // in this case we want the response.data[0].name value
-               // so our object's key is name and the value - the new preferred name - 
-               // is what we want to call it in our code ex cryptoName
-               const { name: cryptoName } = response.data[0]
-               const { logo_url: logo } = response.data[0]
-               const { price: price } = response.data[0]
-               res.render("purchase", {
-                   layout: 'default',
-                   template: 'home-template',
-                   //pass object with additional info to render on index view
-                   // using our new preferred names
-                   name:`${cryptoName}`,
-                   purchaseID : purchase.data,
-                   logo:`${logo}`,
-                   price: parseFloat(`${price}`).toFixed(3)
-               })
-           }).catch((error) => {
-               console.log(error)
-           })
+    //set up API call to get info we want based on value entered
+    const url = `https://api.nomics.com/v1/currencies/ticker?key=${CRYPTO_API_KEY}&ids=${purchaseID}&interval=1d,30d`
+
+    purchase.validateUserInput()
+
+    // use axios to get info from api and do what we want with it in the then
+    axios.get(url).then((response) => {
+        // can use destructuring to get neater names for required info
+        // make a variable with the name of the data point that you want
+        // in this case we want the response.data[0].name value
+        // so our object's key is name and the value - the new preferred name - 
+        // is what we want to call it in our code ex cryptoName
+        const { name: cryptoName } = response.data[0]
+        const { logo_url: logo } = response.data[0]
+        const { price: price } = response.data[0]
+        res.render("purchase", {
+            layout: 'default',
+            template: 'home-template',
+            //pass object with additional info to render on index view
+            // using our new preferred names
+            name: `${cryptoName}`,
+            purchaseID: purchase.data,
+            logo: `${logo}`,
+            price: parseFloat(`${price}`).toFixed(3)
+        })
+    }).catch((error) => {
+        console.log(error)
+    })
 }
 
 //renders the account page with transaction history
-exports.renderAccount = (req, res) =>{
+exports.renderAccount = (req, res) => {
     const user = req.user.email
-    Purchase.find({usernameID: user }).then(history => { 
-        history = history.reverse();   
+    Purchase.find({ usernameID: user }).lean().then(history => {
+        history = history.reverse();
         res.render('account', {
-        name: req.user.name,
-        history
+            name: req.user.name,
+            history
         });
     })
 }
 
-exports.getPurchase = (req, res) =>  {
+exports.getPurchase = (req, res) => {
     //use destructuring to pull required info from purchase page
     const crypto = req.body.name;
     const id = req.body.purchaseID;
@@ -91,11 +91,11 @@ exports.getPurchase = (req, res) =>  {
     const email = req.user.email;
     let errors = [];
 
-    if(quantity <= 0 || ''){
-        errors.push({msg: "Please enter a quantity greater than 0"})
+    if (quantity <= 0 || '') {
+        errors.push({ msg: "Please enter a quantity greater than 0" })
     }
 
-    if(errors.length > 0) {
+    if (errors.length > 0) {
         res.render("purchase", {
             errors,
             name: crypto,
@@ -103,7 +103,7 @@ exports.getPurchase = (req, res) =>  {
             price,
             quantity,
             email
-    });
+        });
     } else {
         //make new purchase schema based on purchase info
         var newPurchase = Purchase({
@@ -114,11 +114,49 @@ exports.getPurchase = (req, res) =>  {
         });
 
         newPurchase.save()
-          .then(purchase => {
-            req.flash('success_msg', 'Transaction successful!');
-            res.redirect('account');
-        })
+            .then(purchase => {
+                req.flash('success_msg', 'Transaction successful!');
+                res.redirect('account');
+            })
     }
 };
 
+exports.sellCrypto = (req, res) => {
+    // Get ID and sellQuantity from account page
+    const crypto_currency = req.body.crypto_currency;
+    const sellQuantity = req.body.sellQuantity
+    const unique_id = req.body.unique_id
+    const purchase_price = req.body.us_dollar
 
+    console.log(req.body.unique_id)
+
+    //set up API call to get info we want based on value entered
+    const url = `https://api.nomics.com/v1/currencies/ticker?key=${CRYPTO_API_KEY}&ids=${crypto_currency}&interval=1d,30d`
+
+    // get current price of crypto by querying the API
+    axios.get(url).then((response) => {
+        // get currentPrice of crypto from the API
+        const currentPrice = response.data[0].price
+        const result = (sellQuantity * purchase_price) - (sellQuantity * currentPrice)
+        console.log(result)
+        // TEST: Commented out condition for testing
+        if (result /*> 0*/) {
+            // if the sale makes money we will update the "gain" property of that purchase
+            // lean removes extra mongoose stuff - makes it cleaner for us - also can't get the _id without it
+            Purchase.findById(unique_id, (purchase => {
+                console.log(purchase);
+                console.log("inside find by id")
+                //purchase.gain = result;
+                //purchase.save();
+            }))
+        }
+            res.render("purchase", {
+                layout: 'default',
+                //pass object with additional info to render on index view
+                // using our new preferred names
+                price: parseFloat(`${price}`).toFixed(3)
+            })
+        }).catch((error) => {
+            console.log(error)
+        })
+}
